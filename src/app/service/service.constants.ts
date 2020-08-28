b919e2502046c57cf543502b1e1dcfa8077260c2
Hemant <hemant97kumar.hk@gmail.com>
2020-08-28 12:09:44 +0530

    Done

--- src/app/service/service.constants.ts
+++ src/app/service/service.constants.ts
@@ -1,6 +1,6 @@
 export class ServiceConstants {
-    public static appContext: string        ="http://15.207.210.221:8080/";
-    //public static appContext: string        ="http://localhost:8080/";
+    //public static appContext: string        ="http://15.207.210.221:8080/";
+    public static appContext: string        ="http://localhost:8080/";
     public static loginUrl: string          = ServiceConstants.appContext+"tour/adminService/login";
     public static tripReportUrl: string     = ServiceConstants.appContext+"tour/api/ver01/tripservice/tripReports";
     public static newTripUrl: string        = ServiceConstants.appContext+"tour/api/ver01/tripservice/trip";
--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 
+/*export interface EditUser{
+  "firstNmae": string;
+  "lastName": string;
+  "email": string;
+  "mobile": string;
+  "oldPass": string;
+  "newPass": string;
+  "confirmPass": string;
+}*/
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
-
+//  editUser: EditUser;
   constructor() { }
 
   ngOnInit() {
+   /* this.editUser={
+      "firstNmae": "",
+      "lastName": "",
+      "email": "",
+      "mobile": "",
+      "oldPass": "",
+      "newPass": "",
+      "confirmPass": ""
+    };*/
   }
-
+ /* updateUser(user, id){
+    this.form.setValue({
+      index: id,
+      firstNmae: user.firstNmae,
+      lastName: user.lastName,
+      email: user.email,
+      mobile: user.mobile,
+      oldPass: user.oldPass,
+      newPass: user.newPass,
+      confirmPass: user.confirmPass
+    })
+  }*/
 }
