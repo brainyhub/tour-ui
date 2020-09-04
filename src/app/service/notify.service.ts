98c2e1a4242635dfccb5ec2752f6f2edfc21c02d
sanjay <sanjay.dhiman82@gmail.com>
2020-09-04 12:20:14 +0530

    fix

---
+++ src/app/service/notify.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from "@angular/core";
+import { ToastrService } from "ngx-toastr";
+
+@Injectable({
+  providedIn: "root",
+})
+export class NotifyService {
+  constructor(private toastrService: ToastrService) {}
+  successMsg(txt: string) {
+    this.toastrService.success("Data shown successfully !!", "Notification");
+  }
+}
