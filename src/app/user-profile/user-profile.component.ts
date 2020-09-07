7ac39d1a25ed38cc3b8428b735b486a75d3e18a5
Hemant <hemant97kumar.hk@gmail.com>
2020-09-07 10:56:35 +0530

    Validation added to edit profile

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,30 +1,15 @@
-<<<<<<< Updated upstream
 import { RegisterUserService } from "./../service/register-user.service";
 import { UpdateUser } from "./UpdateUser";
 import { RegisterUser } from "./../register-user/RegisterUser";
 import { Component, OnInit } from "@angular/core";
 import { NotifyService } from "./../service/notify.service";
 import { FormBuilder, FormGroup, Validators, FormControl} from "@angular/forms";
-=======
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
-import { AdminService } from './../service/admin.service';
-import { Session } from 'protractor';
-import { HttpClient } from '@angular/common/http';
-export interface UserInfo{
-  fName: string;
-  lName: string;
-  email: string;
-  pass: string;
-}
->>>>>>> Stashed changes
 @Component({
   selector: "app-user-profile",
   templateUrl: "./user-profile.component.html",
   styleUrls: ["./user-profile.component.css"],
 })
 export class UserProfileComponent implements OnInit {
-<<<<<<< Updated upstream
   //  editUser: EditUser;
   constructor(private userService: RegisterUserService, private notifyMsg: NotifyService, private fBuilder: FormBuilder) {}
   editMode: Boolean = false;
@@ -37,18 +22,6 @@ export class UserProfileComponent implements OnInit {
   passwordMismatch: Boolean = false;
   setEditMode(flg: Boolean) {
     this.editMode = flg;
-=======
-  info: UserInfo;
-  constructor() { }
-
-  ngOnInit() {
-    this.info={
-      "fName": "",
-      "lName": "",
-      "email": "",
-      "pass": ""
-    }
->>>>>>> Stashed changes
   }
   ngOnInit() {
     this.user = JSON.parse(sessionStorage.getItem("user-info"));
@@ -56,7 +29,6 @@ export class UserProfileComponent implements OnInit {
     console.log(this.user);
     this.setEditData();  
 
-<<<<<<< Updated upstream
     this.updationForm = this.fBuilder.group({
       email: ["", Validators.required],
       password1: ["", [Validators.required, Validators.minLength(8)]],
@@ -101,16 +73,5 @@ export class UserProfileComponent implements OnInit {
         this.notifyMsg.successMsg("Profile updated successfully.","Profile Changed.");
       });
     }
-=======
-  updateUser(){
-    //var data = new Array();
-    var userStore = JSON.parse(sessionStorage.getItem("user-info"));
-    userStore[0] = this.info.fName;
-    userStore[1] = this.info.lName;
-    userStore[2] = this.info.email;
-    userStore[3] = this.info.pass;
-    sessionStorage.setItem("user-info", JSON.stringify(userStore));
-
->>>>>>> Stashed changes
   }
 }
