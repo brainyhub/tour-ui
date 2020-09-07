0ff85cb6eab03a6e876322a894362bc3ce28430f
sanjay <sanjay.dhiman82@gmail.com>
2020-09-07 11:27:23 +0530

    fix

--- src/app/register-user/CheckUser.ts
+++ src/app/register-user/CheckUser.ts
@@ -1,8 +1,10 @@
 export interface CheckUserType{
   userName : string;
+  email : string;
 }
 export class CheckUser implements CheckUserType {
   userName: string;
+  email: string;
   constructor() {
     this.userName = "";
   }
