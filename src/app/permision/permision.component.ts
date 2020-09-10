3be520bd78cbbdacc21357d78044d2cc8f9d223e
Hemant <hemant97kumar.hk@gmail.com>
2020-09-10 09:45:33 +0530

    done

--- src/app/permision/permision.component.ts
+++ src/app/permision/permision.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PermissionService } from "./../service/permission.service";
+import { Permission } from './permission';
 
 @Component({
   selector: 'app-permision',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./permision.component.css']
 })
 export class PermisionComponent implements OnInit {
-
-  constructor() { }
+  permissionData: Permission;
+  constructor(private permissionService: PermissionService) { 
+    this.getPermission();
+  }
 
   ngOnInit() {
   }
-
+  getPermission() {
+    this.permissionService.getAllPermissions().subscribe((data) => {
+      console.log(data);
+      this.permissionData = data;
+    });
+  }
 }
--- src/app/role/role.component.ts
+++ src/app/role/role.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { RoleService } from "./../service/role.service";
 import { Role } from './Role';
+import { RoleType } from './RoleType';
+import { PermissionService } from '../service/permission.service';
+import { Permission } from '../permision/permission';
 
 @Component({
   selector: 'app-role',
@@ -8,16 +11,18 @@ import { Role } from './Role';
   styleUrls: ['./role.component.css']
 })
 export class RoleComponent implements OnInit {
-  constructor(private roleService: RoleService) {}
-  role:  Role = new Role();
-  ngOnInit() {
+  role:  Role;
+  deleteRoleRecord: RoleType[];
+  constructor(private roleService: RoleService) {
     this.getRoles();
+  }
+  ngOnInit() {
+    
   }
   getRoles() {
     this.roleService.getAllRoles().subscribe((data) => {
       console.log(data);
       this.role = data;
-      
     });
   }
 }
